47cbb7f6465040298ced7983e4508492d9285259
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2019-10-25 16:43:46 -0400

    added consign and configure routes folder

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const express = require('express');
+import consign from "consign";
+
+
 const PORT = 3000;
 
 const app = express();
 
 app.set("json spaces", 4);
 
-app.get("/", (req, res) => res.json({ status: "Task Manager API" }));
+consign().include("routes").into(app);
 
-app.get("/tasks", (req, res) => {
-    res.json({
-        tasks: [
-            { title: "Comprar um livro de Javascript" },
-            { title: "Comprar um livro de NodeJS" },
-        ]
-    });
-});
-
-app.listen(PORT, () => console.log(`Task Manager API = Porta ${PORT}`));
+app.listen(PORT, () => console.log(`API Port = ${PORT}`));
