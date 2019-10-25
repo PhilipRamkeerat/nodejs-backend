9ca56d84a1a9dfa44960da72a52547c3b1eb2cf0
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2019-10-25 16:26:49 -0400

    added tasks endpoint and configure json spaces for 4

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const PORT = 3000;
+
 const app = express();
 
+app.set("json spaces", 4);
+
 app.get("/", (req, res) => res.json({ status: "Task Manager API" }));
+
+app.get("/tasks", (req, res) => {
+    res.json({
+        tasks: [
+            { title: "Comprar um livro de Javascript" },
+            { title: "Comprar um livro de NodeJS" },
+        ]
+    });
+});
 
 app.listen(PORT, () => console.log(`Task Manager API = Porta ${PORT}`));
