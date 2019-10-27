ea0cbd851cd695c1ce84ceaa3f85c93bc25d0159
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2019-10-27 18:09:27 -0400

    added tasks initial structure to http verbs

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,28 @@
 module.exports = app => {
     const Tasks = app.db.models.Tasks;
-    app.get("/tasks", (req, res) => {
-        Tasks.findAll({}).then(tasks => {
-            res.json({ tasks: tasks });
+
+    app.route("/tasks")
+        .all((req, res) => {
+            // Middleware pre-execution of routes
+        })
+        .get((req, res) => {
+            // "/tasks": Task List
+        })
+        .post((req, res) => {
+            // "/tasks": Insert a new task
         });
-    });
+
+    app.route("/tasks/:id")
+        .all((req, res) => {
+            // Middle pre-execution of routes
+        })
+        .get((req, res) => {
+            // "/tasks/1" : Find a task
+        })
+        .put((req, res) => {
+            // "/tasks/1": Update a task
+        })
+        .delete((req, res) => {
+            // "/tasks/1": Remove a task
+        });
 };
