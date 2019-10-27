880d13b19199be947b18610da265e2c4e95d1955
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2019-10-27 18:36:42 -0400

    implemented /tasks http verbs

--- routes/tasks.js
+++ routes/tasks.js
@@ -9,9 +9,19 @@ module.exports = app => {
         })
         .get((req, res) => {
             // "/tasks": Task List
+            Tasks.findAll({})
+                .then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({ msg: error.message });
+                });
         })
         .post((req, res) => {
             // "/tasks": Insert a new task
+            Tasks.create(req.body)
+                .then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({ msg: error.message });
+                });
         });
 
     app.route("/tasks/:id")
