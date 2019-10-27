f73a8d5f4e00424a5873c75f170ce7af94c58b5a
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2019-10-27 18:43:33 -0400

    finished crud by id verbs http

--- routes/tasks.js
+++ routes/tasks.js
@@ -32,11 +32,32 @@ module.exports = app => {
         })
         .get((req, res) => {
             // "/tasks/1" : Find a task
+            Tasks.findOne({ where: req.params })
+                .then(result => {
+                    if (result) {
+                        res.json(result);
+                    } else {
+                        res.sendStatus(404);
+                    }
+                })
+                .catch(error => {
+                    res.status(412).json({ msg: error.message });
+                });
         })
         .put((req, res) => {
             // "/tasks/1": Update a task
+            Tasks.update(req.body, { where: req.params })
+                .then(result => res.sendStatus(204))
+                .catch(error => {
+                    res.status(412).json({ msg: error.message });
+                });
         })
         .delete((req, res) => {
             // "/tasks/1": Remove a task
+            Tasks.destroy({ where: req.params })
+                .then(result => res.sendStatus(204))
+                .catch(error => {
+                    res.status(412).json({ msg: error.message });
+                });
         });
 };
